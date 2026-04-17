80d94662142f14d43acb2a7add852a8039587d31
cooksoundscapes <paulobmedina@gmail.com>
2026-04-17 08:55:26 -0300

    added helper text to --help

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,32 @@ int main(int argc, char** argv)
     auto home = getenv("HOME");
     string configPath = home ? string(home) + "/.config/jack-warden/cards.conf" : "";
 
+    app.footer(R"(Registering new sound cards:
+
+1. Monitor udev events and plug your device:
+   udevadm monitor --environment | grep ID_USB_MODEL=
+
+   Copy the value of ID_USB_MODEL.
+
+2. Add it to your config (~/.config/jack-warden/cards.conf):
+   Use the copied value as the "system:" field.
+
+3. Find the ALSA device name:
+   cat /proc/asound/cards
+
+   Identify your device and copy its name.
+
+4. In cards.conf:
+   Use that value as the "device:" field.
+
+Example:
+
+[card]
+system: MY_USB_DEVICE
+device: DeviceName
+
+This allows jack-warden to bind the correct JACK device.)");
+
     app.add_option(
         "-c, --config-file", 
         configPath, 
